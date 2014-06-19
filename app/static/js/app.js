e2e02b299a06156b66cbdf8a06311d64c66c3445
Patrick Facheris <plfacheris@gmail.com>
2014-06-18 22:21:03 -0400

    angularize things and make a cool ass css gauge

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,39 @@
 // Declare app level module which depends on filters, and services
-angular.module('WCGUA.GE', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
-  .config(['$routeProvider', function ($routeProvider) {
+var app = angular.module('WCGUA.GE', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date']);
+
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/home/home.html', 
+        templateUrl: 'views/home/home.html',
         controller: 'HomeController'})
       .otherwise({redirectTo: '/'});
-  }]);
+}]);
+
+app.directive('gaugeCircle', function() {
+    return {
+        link: function(scope, elem, attrs) {
+            var resizeElem;
+            (resizeElem = function () {
+                elem.css('height', (elem.width() / 2) + parseInt(elem.css('border-top-width')));
+                var width = elem.width();
+                elem.css('border-radius', width + 'px ' + width + 'px 0 0');
+            })();
+
+            angular.element(window).bind('resize', resizeElem);
+        }
+    };
+});
+
+app.directive('body', function() {
+    return {
+        restrict: 'E',
+        link: function(scope, elem, attrs) {
+            var resizeElem;
+            (resizeElem = function () {
+                elem.css('height', angular.element(window).height());
+            })();
+
+            angular.element(window).bind('resize', resizeElem);
+        }
+    };
+})
--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -1,3 +1,16 @@
 angular.module('WCGUA.GE')
   .controller('HomeController', ['$scope', function ($scope) {
+    $scope.getAngleStyle = function() {
+        return {
+            '-webkit-transform': 'rotate(' + ($scope.match.percentage / 100) * 180 + 'deg)'
+        };
+    };
+
+    $scope.match = {
+        percentage: 50
+    };
+
+    $scope.set = function(input) {
+        $scope.match.percentage = input;
+    };
   }]);
